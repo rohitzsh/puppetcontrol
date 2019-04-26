c719b07cf898872724ecbbb66c3b151cde9dd8e5
Rohit <rohitsharmazsh@gmail.com>
2019-04-26 11:26:15 +0530

    Adding module minecraft with init.pp class declaration

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,24 @@
+# Class: minecraft
+# Creating minecraft module
+#
+class minecraft {
+    # resources
+    file { '/opt/minecraft':
+        ensure => directory,
+    }
+    file { '/opt/minecraft/server.jar':
+        ensure => file,
+        source => 'https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar'
+    }
+    package { 'java':
+        ensure => present,
+    }
+    file { '/opt/minecraft/eula.txt':
+        ensure  => file,
+        content => 'eula=true',
+    }
+    file { '/etc/systemd/system/minecraft.service':
+        ensure => file,
+        source => 'puppet:///modules/minecraft/minecraft.service',
+    }
+}
