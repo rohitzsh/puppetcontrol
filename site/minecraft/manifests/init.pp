d2d0744cfc2f716e3daa8ebdf8da0a3122a8428a
Rohit <rohitsharmazsh@gmail.com>
2019-04-26 12:03:16 +0530

    Adding parameters and use interpolation to concat it with exiting string

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,19 +2,22 @@
 # Creating minecraft module
 #
 class minecraft {
+    # parameters
+    $url = 'https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar'
+    $install_dir = '/opt/minecraft'
     # resources
-    file { '/opt/minecraft':
+    file { $install_dir:
         ensure => directory,
     }
-    file { '/opt/minecraft/server.jar':
+    file { "${install_dir}/server.jar":
         ensure => file,
-        source => 'https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar'
+        source => $url,
         before => Service['minecraft'],
     }
     package { 'java':
         ensure => present,
     }
-    file { '/opt/minecraft/eula.txt':
+    file { "${install_dir}/eula.txt":
         ensure  => file,
         content => 'eula=true',
     }
@@ -25,6 +28,6 @@ class minecraft {
     service { 'minecraft':
         enable => true,
         ensure => running,
-        require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service'] ]
+        require => [Package['java'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']]
     }
 }
